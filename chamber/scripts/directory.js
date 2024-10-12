28d5d664d3e567a4fa11e00a1432970d3d35b67d
tonnylumwaji <>
2024-10-12 23:16:30 +0300

    do

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,3 +1,4 @@
+document.getElementById("timestamp").innerHTML = Date.now();
 document.getElementById("currentyear").innerHTML = new Date().getFullYear();
 let text = document.lastModified;
 document.getElementById("lastModified").innerHTML = text;
---
+++ chamber/scripts/join.js
@@ -0,0 +1,36 @@
+const openButton = document.querySelector("#openButton")
+const dialogBox = document.querySelector("#dialogBox")
+const closeButton = document.querySelector("#closeButton")
+const dialogBoxText = document.querySelector("#dialogBox div")
+
+
+
+// "Show the dialog" button opens the dialog modal
+openButton1.addEventListener('click', () => {
+  dialogBox.showModal();
+  dialogBoxText.innerHTML = 'This Membership is free!'
+  
+});
+openButton2.addEventListener('click', () => {
+  dialogBox.showModal();
+  dialogBoxText.innerHTML = 'Entry fee is ksh 1000 per month'
+                    ''
+  
+});
+openButton3.addEventListener('click', () => {
+  dialogBox.showModal();
+  dialogBoxText.innerHTML = 'Entry fee is ksh2000'
+  
+});
+
+openButton4.addEventListener('click', () => {
+  dialogBox.showModal();
+  dialogBoxText.innerHTML = 'Entry fee is ksh5000'
+  
+});
+
+
+// Close button closes the dialog
+closeButton.addEventListener('click', () => {
+  dialogBox.close();
+});
